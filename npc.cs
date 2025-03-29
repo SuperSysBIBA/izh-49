2fa720296130e7560119c0844bffc394ef92b95b
SuperSysBIBA <vvkvovavvk@gmail.com>
2025-03-29 17:23:07 +0700

    Add files via upload

---
+++ npc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+    public int health =5;
+public int level =1;
+public float speed=1.2f;
+
+public Vector3 newPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
